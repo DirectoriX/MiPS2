048e7942953a39e029a477dbd411e061b04fb026
DirectoriX <dontsov.mih@yandex.ru>
2015-01-03 23:34:59 +0300

    Ваще норм

--- MiPS2/Form1.cs
+++ MiPS2/Form1.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using Meta.Numerics.Matrices;
 using Meta.Numerics.Statistics.Distributions;
-using Meta.Numerics.Matrices;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
 
 namespace MiPS2
 {
